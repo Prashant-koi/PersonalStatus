20198b466606bbd2843da404619f49a16547cc71
Prashant-koi <koiralaprashanta10@gmail.com>
2025-06-04 22:42:11 -0400

    refactor code strucutre and added overlaywindow abstraact class for cross platform support in futur

---
+++ src/common/OverlayWindow.h
@@ -0,0 +1,32 @@
+// This wil have abstract interface that both Windows and Linux will use
+#ifndef OVERLAY_WINDOW_H
+#define OVERLAY_WINDOW_H
+
+#include <string>
+#include "ThoughtsManager.h"
+
+class OverlayWindow {
+    public:
+
+        //virtual destructur
+        virtual ~OverlayWindow() =default;
+
+        //pure virtual functions
+        virtual bool create() = 0;
+        virtual void show() = 0;
+        virtual void hide() = 0;
+        virtual void messageLoop() = 0;
+        virtual void setThoughtManager(ThoughtsManager* manager) = 0;
+
+        //Factory Method
+        static OverlayWindow* createPlatformWindow();
+
+protected:
+    //Common data in the overlay window
+    ThoughtsManager* thoughtsManager = nullptr;
+    bool isBusyToggled = false;
+
+
+};
+
+#endif
